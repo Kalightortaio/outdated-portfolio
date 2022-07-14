19f0646552e4500b1193f5e61e802e14977ac8fc
Kalightortaio <kokataykrishna@gmail.com>
2022-07-14 15:44:32 -0700

    back to top functionality

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-script-url */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./Footer.css";
 import { Fade } from "react-reveal";
@@ -7,7 +9,8 @@ export default function Footer(props) {
     <div className="footer-div">
       <Fade>
         <p className="footer-text">
-          Developed by Krishna Kokatay
+          Developed by Krishna Kokatay<br/><br/>
+          <a href="javascript:window.scrollTo(0, 0);">Back to Top</a>
         </p>
       </Fade>
     </div>
