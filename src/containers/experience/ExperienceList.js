916a8261da168d83780b7842c92d5c1443a2d21f
Kalightortaio <kokataykrishna@gmail.com>
2022-07-14 13:16:56 -0700

    fixed margin bug

--- src/containers/experience/ExperienceList.js
+++ src/containers/experience/ExperienceList.js
@@ -7,22 +7,24 @@ class ExperienceList extends Component {
   render() {
     const theme = this.props.theme;
     return (
-      <div className="experience-list">
-        {this.props.sections.map((section) => {
-          return (
-            <Card
-              className="experience-card"
-              title={section["title"]}
-              key={section["title"]}
-            >
-              {section["experiences"].map((experience, index) => {
-                return (
-                  <ExperienceCard experience={experience} theme={theme} key={index}/>
-                );
-              })}
-            </Card>
-          );
-        })}
+      <div className="experience-list-container">
+        <div className="experience-list">
+          {this.props.sections.map((section) => {
+            return (
+              <Card
+                className="experience-card"
+                title={section["title"]}
+                key={section["title"]}
+              >
+                {section["experiences"].map((experience, index) => {
+                  return (
+                    <ExperienceCard experience={experience} theme={theme} key={index}/>
+                  );
+                })}
+              </Card>
+            );
+          })}
+        </div>
       </div>
     );
   }
